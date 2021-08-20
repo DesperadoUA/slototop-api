16ef4ee7390e8b61e52d0e06b00f104788829ba4
lazarev <k.lazarev@cleack.com>
2021-08-20 11:55:27 +0300

    Add language

---
+++ database/migrations/2014_10_12_100000_language_category.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LanguageCategory extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('language_category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('parent_id')->default(0);
+            $table->string('post_type')->default('language_category');
+            $table->enum('status', ['public', 'hide', 'basket'])->default('public');
+            $table->string('permalink');
+            $table->string('slug')->default('category');
+            $table->string('title');
+            $table->string('thumbnail', 300);
+            $table->string('short_desc', 500);
+            $table->string('h1');
+            $table->string('meta_title');
+            $table->string('description');
+            $table->string('keywords');
+            $table->longText('content');
+            $table->integer('lang')->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->unique('permalink');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('language_category');
+    }
+}
