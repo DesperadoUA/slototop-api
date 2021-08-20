710f243fe129389f8e212ccda8b622627e14570f
lazarev <k.lazarev@cleack.com>
2021-08-20 15:51:32 +0300

    Add Vendors

---
+++ database/migrations/2014_10_12_100001_vendor_meta.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class VendorMeta extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendor_meta', function (Blueprint $table) {
+            $table->bigInteger('post_id')->unsigned();
+            $table->integer('rating');
+            $table->string('year');
+            $table->string('license');
+            $table->string('number_games');
+            $table->unique('post_id');
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('vendors')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendor_meta');
+    }
+}
