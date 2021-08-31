70f21e88d21d693cdc3a7edec0e0f8b89b52be7a
lazarev <k.lazarev@cleack.com>
2021-08-31 12:02:52 +0300

    Add bonus category

--- database/migrations/2014_10_12_100000_bonus_category.php
+++ database/migrations/2014_10_12_100000_bonus_category.php
@@ -16,10 +16,10 @@ class BonusCategory extends Migration
         Schema::create('bonus_category', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('parent_id')->default(0);
-            $table->string('post_type')->default('bonus_category');
+            $table->string('post_type')->default('bonus/category');
             $table->enum('status', ['public', 'hide', 'basket'])->default('public');
             $table->string('permalink');
-            $table->string('slug')->default('category');
+            $table->string('slug')->default('bonus/category');
             $table->string('title');
             $table->string('thumbnail', 300);
             $table->string('short_desc', 500);
--- routes/api.php
+++ routes/api.php
@@ -67,6 +67,13 @@ Route::namespace('Api')->group(function () {
     Route::post('admin/bonus/update', 'AdminBonusController@update')->middleware('api_auth');
     Route::post('admin/bonus/delete', 'AdminBonusController@delete')->middleware('api_auth');
     Route::post('admin/bonus/store', 'AdminBonusController@store')->middleware('api_auth');
+
+    Route::post('admin/bonus/category', 'AdminBonusCategoryController@index')->middleware('api_auth');
+    Route::post('admin/bonus/category/update', 'AdminBonusCategoryController@update')->middleware('api_auth');
+    Route::post('admin/bonus/category/delete', 'AdminBonusCategoryController@delete')->middleware('api_auth');
+    Route::post('admin/bonus/category/store', 'AdminBonusCategoryController@store')->middleware('api_auth');
+    Route::post('admin/bonus/category/{id}', 'AdminBonusCategoryController@show')->middleware('api_auth');
+
     Route::post('admin/bonus/{id}', 'AdminBonusController@show')->middleware('api_auth');
 
     Route::post('admin/countries', 'AdminCountryController@index')->middleware('api_auth');
