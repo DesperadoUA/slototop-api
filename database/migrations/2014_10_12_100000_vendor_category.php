1d6f8e3e3669abd0c7a4d6cacf7adcc42b7ef975
lazarev <k.lazarev@cleack.com>
2021-09-01 11:07:10 +0300

    Add vendor category

--- database/migrations/2014_10_12_100000_vendor_category.php
+++ database/migrations/2014_10_12_100000_vendor_category.php
@@ -16,10 +16,10 @@ class VendorCategory extends Migration
         Schema::create('vendor_category', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('parent_id')->default(0);
-            $table->string('post_type')->default('vendor_category');
+            $table->string('post_type')->default('vendor/category');
             $table->enum('status', ['public', 'hide', 'basket'])->default('public');
             $table->string('permalink');
-            $table->string('slug')->default('category');
+            $table->string('slug')->default('vendor/category');
             $table->string('title');
             $table->string('thumbnail', 300);
             $table->string('short_desc', 500);
