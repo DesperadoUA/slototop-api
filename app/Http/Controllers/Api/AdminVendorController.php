088413989766c16b0f8f2de46f009c4c5cc5b4d1
lazarev <k.lazarev@cleack.com>
2021-09-01 16:22:01 +0300

    Add relative category for vendors

--- app/Http/Controllers/Api/AdminVendorController.php
+++ app/Http/Controllers/Api/AdminVendorController.php
@@ -12,6 +12,8 @@ class AdminVendorController extends BaseController
     const POST_TYPE = 'vendor';
     const MAIN_TABLE = 'vendors';
     const META_TABLE = 'vendor_meta';
+    const CATEGORY_TABLE = 'vendor_category';
+    const CATEGORY_RELATIVE = 'vendor_category_relative';
 
     public function index(Request $request)
     {
@@ -67,6 +69,9 @@ class AdminVendorController extends BaseController
         $data = $post->getPostById($id);
         if (!empty(count($data))) {
             $response['body'] = self::dataCommonDecode($data[0]) + self::dataMetaDecode($data[0]);
+            $response['body']['category'] = self::relativeCategoryPost($id, self::MAIN_TABLE,
+                                                                         self::CATEGORY_TABLE,
+                                                                          self::CATEGORY_RELATIVE);
             $response['confirm'] = 'ok';
         }
 
@@ -87,7 +92,9 @@ class AdminVendorController extends BaseController
 
         $data_meta = self::dataValidateMetaSave($data_request);
         $post->updateMetaById($data_request['id'], $data_meta);
-        //self::updateCategory($data_request['id'], $data_request['category']);
+        self::updateCategory($data_request['id'], $data_request['category'], self::MAIN_TABLE,
+                                                                          self::CATEGORY_TABLE,
+                                                                           self::CATEGORY_RELATIVE);
 
         return response()->json($response);
     }
